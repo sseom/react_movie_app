a7ce609ec7f7eb963fcdca4f19b81d83e5d57ecf
summer <summer.time@kakaocorp.com>
2021-03-16 17:58:09 +0900

    04.무비앱만들기 - ROUTING 2
    - Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./components/Navigation";
+import './App.css';
 
-// state를 이용하기 위해서 className component를 사용
 function App() {
-  return <span>something</span>;
+  return (
+    <HashRouter>
+      {/* 모든걸 HashRouter 안에 넣을 필요는 없지만  
+      link는 route안에 있어야함 */}
+      <Navigation /> 
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+    
+    /* 
+      BrowserRouter VS HashRouter
+      별차이 없음 단 /#/ 해시태그의 차이가 있고 gh-pages에 설정하기 복잡함
+      gh-pages에 업로드할예정이니 HashRouter을 사용
+
+      <BrowserRouter>
+        <Navigation />
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+      </BrowserRouter>
+    */
+  );
 }
 
 export default App;
